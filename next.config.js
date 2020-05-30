07eecdf56e47d27288718c3d6498158d173b2bc6
Mikkel Laursen <mlaursen03@gmail.com>
2020-05-29 23:26:49 -0600

    Fixed asset prefix after moving to mlaursen.com

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProd ? '/mlaursen.com/' : '',
   exportPathMap: () => ({
     '/': { page: '/' },
   }),
