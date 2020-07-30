3de2f61e1c12f2df542fbdd90b5564c78a6a20ae
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-29 18:03:41 -0600

    fix: Fixed custom document
    Error:
    Error occurred prerendering page "/". Read more: https://err.sh/next.js/prerender-error
    Error: "Document.getInitialProps()" should resolve to an object with a "html" prop set with a valid html string
        at renderToHTML (/Users/mlaursen/code/mlaursen.com/node_modules/next/dist/next-server/server/render.js:46:254)
        at async Object.exportPage [as default] (/Users/mlaursen/code/mlaursen.com/node_modules/next/dist/export/worker.js:18:6)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,31 +1,33 @@
 import React, { ReactElement } from "react";
-import { Head, Main, NextScript, Html } from "next/document";
+import Document, { Head, Main, NextScript, Html } from "next/document";
 
-export default function Document(): ReactElement {
-  return (
-    <Html lang="en" dir="lrt">
-      <Head>
-        <meta name="theme-color" content="#000000" />
-        <meta name="og:title" content="Mikkel Laursen" />
-        <meta name="og:url" content="/" />
-        <meta
-          name="og:description"
-          content="A placeholder portfolio website for Mikkel Laursen"
-        />
-        <meta
-          name="description"
-          content="A placeholder portfolio website for Mikkel Laursen"
-        />
-        <link
-          key="font"
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  public render(): ReactElement {
+    return (
+      <Html lang="en" dir="lrt">
+        <Head>
+          <meta name="theme-color" content="#000000" />
+          <meta name="og:title" content="Mikkel Laursen" />
+          <meta name="og:url" content="/" />
+          <meta
+            name="og:description"
+            content="A placeholder portfolio website for Mikkel Laursen"
+          />
+          <meta
+            name="description"
+            content="A placeholder portfolio website for Mikkel Laursen"
+          />
+          <link
+            key="font"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
