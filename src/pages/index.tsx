cff8025a50565c28b254a38115225a81b552e6d9
Mikkel Laursen <mlaursen03@gmail.com>
2020-10-12 17:52:03 -0600

    chore: updated to use my shareable eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,17 @@ import { TextContainer, Text } from "@react-md/typography";
 
 import styles from "./index.module.scss";
 
-export default (): ReactElement => (
-  <TextContainer className={styles.container}>
-    <Text type="headline-2" margin="none">
-      Hello,
-    </Text>
-    <Text type="headline-4">
-      This page is empty since I haven't really made this site yet. You can
-      check out my <Link href="https://github.com/mlaursen">GitHub</Link> for
-      some more information about projects I work on.
-    </Text>
-  </TextContainer>
-);
+export default function Home(): ReactElement {
+  return (
+    <TextContainer className={styles.container}>
+      <Text type="headline-2" margin="none">
+        Hello,
+      </Text>
+      <Text type="headline-4">
+        This page is empty since I haven&apos;t really made this site yet. You
+        can check out my <Link href="https://github.com/mlaursen">GitHub</Link>{" "}
+        for some more information about projects I work on.
+      </Text>
+    </TextContainer>
+  );
+}
