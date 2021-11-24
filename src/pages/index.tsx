f30947fe48115e9a1ca8606635992e15bd38281f
Mikkel Laursen <mlaursen03@gmail.com>
2021-11-24 16:38:26 -0700

    chore(deps): bump react-md to v4.0.0 and enable React.StrictMode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
 import type { ReactElement } from "react";
 import { Link } from "@react-md/link";
-import { TextContainer, Text } from "@react-md/typography";
+import { TextContainer, Typography } from "@react-md/typography";
 
 import styles from "./index.module.scss";
 
 export default function Home(): ReactElement {
   return (
     <TextContainer className={styles.container}>
-      <Text type="headline-2" margin="none">
+      <Typography type="headline-2" margin="none">
         Hello,
-      </Text>
-      <Text type="headline-4">
+      </Typography>
+      <Typography type="headline-4">
         This page is empty since I haven&apos;t really made this site yet. You
         can check out my <Link href="https://github.com/mlaursen">GitHub</Link>{" "}
         for some more information about projects I work on.
-      </Text>
+      </Typography>
     </TextContainer>
   );
 }
