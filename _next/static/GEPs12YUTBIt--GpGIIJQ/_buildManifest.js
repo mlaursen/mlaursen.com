ec5891ea6ecb9c3cd33ccf06721288d9d149e3db
mlaursen <mlaursen03@gmail.com>
2022-05-05 02:36:00 +0000

    Deploying to gh-pages from @ mlaursen/mlaursen.com@ffa438ea648b490d1051c8c33c1c62b95f35e3a5 🚀

---
+++ _next/static/GEPs12YUTBIt--GpGIIJQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6c03467bd7c1d778.css","static/chunks/pages/index-2f39793c40b17c8d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
