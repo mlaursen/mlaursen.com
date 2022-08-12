c7e10216eaabbb0febcf7560d2307850fecac107
Mikkel Laursen <mlaursen03@gmail.com>
2022-08-12 16:42:42 -0600

    build(deps): bump dependencies to latest

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
@@ -6,8 +5,8 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.ts").default(on, config);
+    setupNodeEvents(_on, config) {
+      return config;
     },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
