ae92623dee76a2f6a5f65032b0c3a586ba8dfe07
Mikkel Laursen <mlaursen03@gmail.com>
2023-01-17 19:41:25 -0700

    build(dev-deps): bump eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,5 @@ module.exports = {
   extends: ["@mlaursen/eslint-config", "plugin:@next/next/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": ["error"],
-
-    "no-use-before-define": 0,
-    "@typescript-eslint/no-use-before-define": [
-      "warn",
-      { ignoreTypeReferences: true },
-    ],
-
-    "object-shorthand": ["error", "always"],
   },
 };
