1672fb0318e62d2a6a56ad8dae348cef9357e519
Mikkel Laursen <mlaursen03@gmail.com>
2023-01-17 19:17:07 -0700

    build(deps): bump dependencies to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['@mlaursen/eslint-config', 'plugin:@next/next/recommended'],
+  root: true,
+  extends: ["@mlaursen/eslint-config", "plugin:@next/next/recommended"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": ["error"],
+
+    "no-use-before-define": 0,
+    "@typescript-eslint/no-use-before-define": [
+      "warn",
+      { ignoreTypeReferences: true },
+    ],
+
+    "object-shorthand": ["error", "always"],
+  },
 };
