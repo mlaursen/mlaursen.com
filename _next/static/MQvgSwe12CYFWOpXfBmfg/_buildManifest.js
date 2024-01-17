016636372c0608f0a098f4d516786500e1e2682c
mlaursen <mlaursen03@gmail.com>
2024-01-17 22:06:07 +0000

    Deploying to gh-pages from @ mlaursen/mlaursen.com@d50e763bcdc92129b598979ee5cca584b897746f 🚀

---
+++ _next/static/MQvgSwe12CYFWOpXfBmfg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-bc9cd9a8c03b7b73.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
