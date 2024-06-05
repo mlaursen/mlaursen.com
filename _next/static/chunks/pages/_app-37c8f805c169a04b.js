28b59907c5e61c5e9daf1f0ea8c930ae0686066e
mlaursen <mlaursen03@gmail.com>
2024-06-05 14:57:43 +0000

    Deploying to gh-pages from @ mlaursen/mlaursen.com@75c62e96454a5613ccc5d93524925cfb9975a377 🚀

---
+++ _next/static/chunks/pages/_app-37c8f805c169a04b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{199:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(8152)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(199),_(5634)}),_N_E=n.O()}]);
