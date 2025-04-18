6cc39edce87c823aa8dd399a814ca13bbf52ad4e
Mikkel Laursen <mlaursen03@gmail.com>
2025-04-18 11:16:48 -0400

    build(deps): update eslint config to latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
+import { FlatCompat } from "@eslint/eslintrc";
 import { config, configs, gitignore } from "@mlaursen/eslint-config";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 export default config(
   gitignore(import.meta.url),
-  ...configs.frontendTypeChecking(import.meta.dirname),
-  ...configs.next,
+  ...compat.config({
+    extends: ["plugin:@next/next/core-web-vitals"],
+  }),
+  ...configs.frontendTypeChecking(import.meta.dirname, "jest"),
   {
     files: ["tests/**/*.spec.ts"],
     rules: {
