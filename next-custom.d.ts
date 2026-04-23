4425541096ce885d45ce3c863fa25b2773579ce5
Mikkel Laursen <mlaursen03@gmail.com>
2026-04-23 19:42:08 -0400

    build: remove next-custom.d.ts

--- next-custom.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "next/font/google" {
-  export * from "next/font/google/index.js";
-}
--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import { type NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   sassOptions: {
     implementation: "sass-embedded",
   },
